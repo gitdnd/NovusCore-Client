3d02c62066660099d2c12477cc3a8f8af7789164
NixAJ <nickiarpejensen@gmail.com>
2021-10-22 21:42:29 +0200

    Added more functions to TimeAPI
    Updated Submodule NovusCore-Common

--- client/Scripting/API/Gameplay/Time/TimeAPI.cpp
+++ client/Scripting/API/Gameplay/Time/TimeAPI.cpp
@@ -3,6 +3,7 @@
 #include "../../../ScriptAPI.h"
 #include "../../../../Utils/ServiceLocator.h"
 #include "../../../../ECS/Components/Singletons/DayNightSingleton.h"
+#include "../../../../ECS/Components/Singletons/TimeSingleton.h"
 
 #include <entt.hpp>
 #include <Nai/Compiler/Compiler.h>
@@ -14,7 +15,36 @@ namespace ScriptingAPI
         bool GetTimeCallback(Interpreter* interpreter)
         {
             entt::registry* registry = ServiceLocator::GetGameRegistry();
+            TimeSingleton& timeSingleton = registry->ctx<TimeSingleton>();
 
+            f64 timeInSecond = timeSingleton.lifeTimeInS;
+            interpreter->SetReturnValue<f64>(&timeInSecond);
+
+            return true;
+        }
+        bool GetTimeInMSCallback(Interpreter* interpreter)
+        {
+            entt::registry* registry = ServiceLocator::GetGameRegistry();
+            TimeSingleton& timeSingleton = registry->ctx<TimeSingleton>();
+
+            f64 timeInSecond = timeSingleton.lifeTimeInMS;
+            interpreter->SetReturnValue<f64>(&timeInSecond);
+
+            return true;
+        }
+        bool GetDeltaTimeCallback(Interpreter* interpreter)
+        {
+            entt::registry* registry = ServiceLocator::GetGameRegistry();
+            TimeSingleton& timeSingleton = registry->ctx<TimeSingleton>();
+
+            f64 timeInSecond = timeSingleton.deltaTime;
+            interpreter->SetReturnValue<f64>(&timeInSecond);
+
+            return true;
+        }
+        bool GetTimeOfDayCallback(Interpreter* interpreter)
+        {
+            entt::registry* registry = ServiceLocator::GetGameRegistry();
             DayNightSingleton& dayNightSingleton = registry->ctx<DayNightSingleton>();
 
             f64 timeInSecond = dayNightSingleton.seconds;
@@ -29,6 +59,9 @@ namespace ScriptingAPI
             scriptAPI->RegisterModule(module);
 
             NativeFunction nfGetTime(cc, module, "GetTime", GetTimeCallback);
+            NativeFunction nfGetTimeInMS(cc, module, "GetTimeInMS", GetTimeInMSCallback);
+            NativeFunction nfGetDeltaTime(cc, module, "GetDeltaTime", GetDeltaTimeCallback);
+            NativeFunction nfGetTimeOfDay(cc, module, "GetTimeOfDay", GetTimeOfDayCallback);
         }
 
         void Init(ScriptAPI* scriptAPI)
